c2791fa72f1ec2ab695def358f770787baf5fa80
Helder <helder@lamoulade.com>
2015-10-14 11:46:11 +0100

    boilerplate code

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>...</title>
+
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+		<link rel="stylesheet" href="css/app.css">
+	</head>
+
+	<body>
+
+		<!-- start: namespace module -->
+		<div class="namespace">
+
+			<!-- start: canvas -->
+			<canvas></canvas>
+			<!-- end: canvas -->
+
+		</div>
+		<!-- end: namespace module -->
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.1.8/imagesloaded.pkgd.min.js"></script>
+		<script src="js/vendor/tap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenLite.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/easing/EasePack.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+
+</html>
